267e2e5a5b4d407ebd5167ed67eb894a07ffb98e
admin <admin@PC5>
2018-04-25 17:16:11 +0530

    Add project files.

---
+++ Particle/ParticlePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Particle
+{
+	[Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
+	public partial class ParticlePage : ContentPage
+	{
+		public ParticlePage()
+		{
+			InitializeComponent();
+
+		}
+
+	    private void Button_OnClicked(object sender, EventArgs e)
+	    {
+	        Navigation.PushAsync(new Page2());
+	    }
+	}
+}
